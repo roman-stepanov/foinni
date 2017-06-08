6fec39b64e0649cf47216301b551516e4a88ba0d
Roman Stepanov <romik112@yandex.ru>
2017-06-08 20:23:51 +0300

    Сделал разметку для блока intro

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,9 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           src: [
-            '*.html'
+            '*.html',
+            'img/**/*.{jpg,png}',
+            'fonts/**.{woff,woff2}'
           ],
           dest: 'build'
         }]
